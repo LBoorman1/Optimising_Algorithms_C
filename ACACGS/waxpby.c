15e366f7a4514a8c921fe0016d8eb47ae022d901
Luke Boorman <Luke.Boorman@warwick.ac.uk>
2022-02-24 13:14:41 +0000

    first attempt

--- ACACGS/waxpby.c
+++ ACACGS/waxpby.c
@@ -1,4 +1,5 @@
 #include "waxpby.h"
+#include "immintrin.h"
 
 /**
  * @brief Compute the update of a vector with the sum of two scaled vectors
@@ -11,17 +12,46 @@
  * @param w Output vector
  * @return int 0 if no error
  */
+
+
+
+//leave n alone
+//need to do loop unrolling, means creating a loop factor and incrementing i by the loop factor each iteration
+//double is 64 bits, so can only fit two into a 128 bit vector, use this fact to find a loop factor
+
 int waxpby (const int n, const double alpha, const double * const x, const double beta, const double * const y, double * const w) {  
+  //creating loopN and loopFactor
+  int loopFactor = 2;
+  int loopN = (n/loopFactor)*loopFactor;
+
   if (alpha==1.0) {
-    for (int i=0; i<n; i++) {
+    int i;
+    for (int i=0; i<loopN; i+=loopFactor) {
       w[i] = x[i] + beta * y[i];
+      w[i+1] = x[i+1] + beta * y[i+1];
     }
+    //remaining elements
+    for (; i<n; i++){
+      w[i] = x[i] + beta + y[i];
+    }
+
   } else if(beta==1.0) {
-    for (int i=0; i<n; i++) {
+    int i;
+    for (int i=0; i<loopN; i+=loopFactor) {
+      w[i] = alpha * x[i] + y[i];
+      w[i+1] = alpha * x[i+1] + y[i+1];
+    }
+    for (; i<n; i++) {
       w[i] = alpha * x[i] + y[i];
     }
+
   } else {
-    for (int i=0; i<n; i++) {
+    int i;
+    for (int i=0; i<loopN; i+=loopFactor) {
+      w[i] = alpha * x[i] + beta * y[i];
+      w[i+1] = alpha * x[i] + beta * y[i+1];
+    }
+    for (; i<n; i++) {
       w[i] = alpha * x[i] + beta * y[i];
     }
   }
