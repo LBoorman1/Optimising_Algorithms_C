c126cb7bd27709f575a2297efe14aab01380ca29
Luke Boorman <Luke.Boorman@warwick.ac.uk>
2022-03-10 22:19:33 +0000

    first optimisations to waxpby.c

--- ACACGS/waxpby.c
+++ ACACGS/waxpby.c
@@ -1,5 +1,6 @@
 #include "waxpby.h"
-#include "immintrin.h"
+#include <immintrin.h>
+#include <omp.h>
 
 /**
  * @brief Compute the update of a vector with the sum of two scaled vectors
@@ -14,47 +15,24 @@
  */
 
 
-
-//leave n alone
-//need to do loop unrolling, means creating a loop factor and incrementing i by the loop factor each iteration
-//double is 64 bits, so can only fit two into a 128 bit vector, use this fact to find a loop factor
-
 int waxpby (const int n, const double alpha, const double * const x, const double beta, const double * const y, double * const w) {  
-  //creating loopN and loopFactor
+  
+  int i;
   int loopFactor = 2;
   int loopN = (n/loopFactor)*loopFactor;
+  
+  __m128d vectorB = _mm_set1_pd(beta);
+  __m128d vectorA = _mm_set1_pd(alpha);
 
-  if (alpha==1.0) {
-    int i;
-    for (int i=0; i<loopN; i+=loopFactor) {
-      w[i] = x[i] + beta * y[i];
-      w[i+1] = x[i+1] + beta * y[i+1];
-    }
-    //remaining elements
-    for (; i<n; i++){
-      w[i] = x[i] + beta + y[i];
-    }
-
-  } else if(beta==1.0) {
-    int i;
-    for (int i=0; i<loopN; i+=loopFactor) {
-      w[i] = alpha * x[i] + y[i];
-      w[i+1] = alpha * x[i+1] + y[i+1];
-    }
-    for (; i<n; i++) {
-      w[i] = alpha * x[i] + y[i];
-    }
-
-  } else {
-    int i;
-    for (int i=0; i<loopN; i+=loopFactor) {
-      w[i] = alpha * x[i] + beta * y[i];
-      w[i+1] = alpha * x[i] + beta * y[i+1];
-    }
-    for (; i<n; i++) {
-      w[i] = alpha * x[i] + beta * y[i];
-    }
+  #pragma omp for
+  for(i=0; i < loopN; i += loopFactor){
+    __m128d vectorX = _mm_load_pd(x+i);
+    __m128d vectorY = _mm_load_pd(y+i);
+    __m128d vectorXa = _mm_mul_pd(vectorX, vectorA);
+    __m128d vectorYb = _mm_mul_pd(vectorY, vectorB);
+    __m128d vectorW = _mm_add_pd(vectorYb, vectorXa);
+    _mm_store_pd(w+i, vectorW);
   }
-
   return 0;
 }
+//The loop is very inefficient because the else encapsulates the whole loop essentially
