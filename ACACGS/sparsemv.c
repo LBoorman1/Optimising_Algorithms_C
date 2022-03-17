9803e857af12018e20ed92acefd3be806f0a2088
Luke Boorman <Luke.Boorman@warwick.ac.uk>
2022-03-17 18:18:21 +0000

    up to date

--- ACACGS/sparsemv.c
+++ ACACGS/sparsemv.c
@@ -2,6 +2,10 @@
 #include <ctype.h>
 #include <assert.h>
 #include <math.h>
+#include <immintrin.h>
+#include <omp.h>
+#include <stdio.h>
+
 
 #include "sparsemv.h"
 
@@ -21,12 +25,14 @@ int sparsemv(struct mesh *A, const double * const x, double * const y)
 
   const int nrow = (const int) A->local_nrow; //number of rows in the A matrix
 
+  #pragma omp for
   for (int i=0; i< nrow; i++) {
       double sum = 0.0; //needs to be inside loop so it is set to 0 for every row
-      const double * const cur_vals = (const double * const) A->ptr_to_vals_in_row[i]; //array of values in the row
-      const int * const cur_inds = (const int * const) A->ptr_to_inds_in_row[i]; //array of 
+      const double * const cur_vals = (const double * const) A->ptr_to_vals_in_row[i]; //array of non-zero values 
+      const int * const cur_inds = (const int * const) A->ptr_to_inds_in_row[i]; //array of indexes where the non-zero values are
       const int cur_nnz = (const int) A->nnz_in_row[i]; //number of non-zeros in the row
 
+      //#pragma omp parallel for reduction (+:sum)
       for (int j=0; j< cur_nnz; j++) {
         sum += cur_vals[j]*x[cur_inds[j]];
       }
