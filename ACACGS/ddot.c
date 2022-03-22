c0d4ef184c4e02d7236d3560246dfe82e63996ec
Luke Boorman <Luke.Boorman@warwick.ac.uk>
2022-03-22 22:00:20 +0000

    This is the final time

--- ACACGS/ddot.c
+++ ACACGS/ddot.c
@@ -11,28 +11,32 @@
  * @param result Pointer to scalar result value
  * @return int 0 if no error
  */
-int ddot (const int n, const double * const x, const double * const y, double * const result) {  
+int ddot (const int n, const float * const x, const float * const y, float * const result) {  
   
   int i;
-  int loopFactor = 2;
+  int loopFactor = 4;
   int loopN = (n/loopFactor)*loopFactor;
 
   //vectorising
-  double local_result = 0.0;
+  float local_result = 0.0;
 
 
   #pragma omp parallel for reduction(+:local_result)
   for (i=0; i<loopN; i+=loopFactor) {
     
-    __m128d vectorX = _mm_load_pd(x+i);
-    __m128d vectorY = _mm_load_pd(y+i);
-    __m128d vectorXY = _mm_mul_pd(vectorX, vectorY);
-    __m128d addVec = _mm_hadd_pd(vectorXY,vectorXY);
-    local_result+= _mm_cvtsd_f64(addVec);
+    __m128 vectorX = _mm_load_ps(x+i);
+    __m128 vectorY = _mm_load_ps(y+i);
+    __m128 vectorXY = _mm_mul_ps(vectorX, vectorY);
+    __m128 addVec = _mm_hadd_ps(vectorXY,vectorXY);
+    __m128 newAddVec = _mm_hadd_ps(addVec, addVec);
+    local_result+= _mm_cvtss_f32(newAddVec);
   }
-  if (n%2 == 1){
-    local_result += x[n-1]*y[n-1];
+  for(i=loopN; i < n; i++){
+    local_result += x[i]*y[i];
   }
+  // if (n%2 == 1){
+  //   local_result += x[n-1]*y[n-1];
+  // }
 
   *result = local_result;
 
--- ACACGS/sparsemv.c
+++ ACACGS/sparsemv.c
@@ -18,25 +18,25 @@
  * @return int 0 if no error
  */
 
-double sum256vector(__m256d vector) {
-  __m128d lowhalf = _mm256_castpd256_pd128(vector);
-  __m128d highhalf = _mm256_extractf128_pd(vector, 1);
-  lowhalf = _mm_add_pd(lowhalf, highhalf);
-  __m128d hightest = _mm_unpackhi_pd(lowhalf, lowhalf);
-  __m128d vectorAdd = _mm_add_sd(lowhalf, hightest); 
-  return _mm_cvtsd_f64(vectorAdd);
-}
+// float sum256vector(__m256d vector) {
+//   __m128d lowhalf = _mm256_castpd256_pd128(vector);
+//   __m128d highhalf = _mm256_extractf128_pd(vector, 1);
+//   lowhalf = _mm_add_pd(lowhalf, highhalf);
+//   __m128d hightest = _mm_unpackhi_pd(lowhalf, lowhalf);
+//   __m128d vectorAdd = _mm_add_sd(lowhalf, hightest); 
+//   return _mm_cvtsd_f64(vectorAdd);
+// }
 
 //Basically doing matrix multiplication A*x
-int sparsemv(struct mesh *A, const double * const x, double * const y)
+int sparsemv(struct mesh *A, const float * const x, float * const y)
 {
 
   const int nrow = (const int) A->local_nrow; //number of rows in the A matrix
 
   #pragma omp parallel for
   for (int i=0; i< nrow; i++) {
-      double sum = 0.0; //needs to be inside loop so it is set to 0 for every row
-      const double * const cur_vals = (const double * const) A->ptr_to_vals_in_row[i]; //array of non-zero values 
+      float sum = 0.0; //needs to be inside loop so it is set to 0 for every row
+      const float * const cur_vals = (const float * const) A->ptr_to_vals_in_row[i]; //array of non-zero values 
       const int * const cur_inds = (const int * const) A->ptr_to_inds_in_row[i]; //array of indexes where the non-zero values are
       const int cur_nnz = (const int) A->nnz_in_row[i]; //number of non-zeros in the row - works as n for inner loop
 
@@ -46,12 +46,21 @@ int sparsemv(struct mesh *A, const double * const x, double * const y)
       //#pragma omp parallel for reduction (+:sum)
       for (int j=0; j<loopN; j+=loopFactor) {
         
-        __m256d vectorValues = _mm256_load_pd(cur_vals + j);
-        __m256d vectorXVals = _mm256_set_pd(x[cur_inds[j+3]], x[cur_inds[j+2]], x[cur_inds[j+1]], x[cur_inds[j]]);
-        __m256d vectorMul = _mm256_mul_pd(vectorValues, vectorXVals);
-        double scalarAdd = sum256vector(vectorMul);
+        // __m256d vectorValues = _mm256_load_pd(cur_vals + j);
+        // __m256d vectorXVals = _mm256_set_pd(x[cur_inds[j+3]], x[cur_inds[j+2]], x[cur_inds[j+1]], x[cur_inds[j]]);
+        // __m256d vectorMul = _mm256_mul_pd(vectorValues, vectorXVals);
+        // float scalarAdd = sum256vector(vectorMul);
 
-        sum += scalarAdd;
+        // sum += scalarAdd;
+
+        __m128 vectorVals = _mm_loadu_ps(cur_vals + j);
+        __m128 vectorX = _mm_set_ps(x[cur_inds[j+3]], x[cur_inds[j+2]], x[cur_inds[j+1]], x[cur_inds[j]]);
+        __m128 vectorValsX = _mm_mul_ps(vectorVals, vectorX);
+        __m128 vectorAdd = _mm_hadd_ps(vectorValsX, vectorValsX);
+        __m128 vectorNewAdd = _mm_hadd_ps(vectorAdd, vectorAdd);
+
+        sum+=_mm_cvtss_f32(vectorNewAdd);
+
       }
       for(int j=loopN; j<cur_nnz; j++){
         sum+= cur_vals[loopN]*x[cur_inds[j]];
