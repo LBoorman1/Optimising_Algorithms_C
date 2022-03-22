081d3aafe4101bbbde0aeb52670d96bc49b65d2a
Luke Boorman <Luke.Boorman@warwick.ac.uk>
2022-03-22 17:58:25 +0000

    fixed waxpby.c sill slower though

--- ACACGS/waxpby.c
+++ ACACGS/waxpby.c
@@ -18,24 +18,24 @@
 int waxpby (const int n, const float alpha, const float * const x, const float beta, const float * const y, float * const w) {  
   
   int i;
-  int loopFactor = 8;
+  int loopFactor = 4;
   int loopN = (n/loopFactor)*loopFactor;
   
-  __m256 vectorB = _mm256_set1_ps(beta);
-  __m256 vectorA = _mm256_set1_ps(alpha);
+  __m128 vectorB = _mm_set1_ps(beta);
+  __m128 vectorA = _mm_set1_ps(alpha);
 
   #pragma omp parallel for
   for(i=0; i < loopN; i += loopFactor){
-    __m256 vectorX = _mm256_load_ps(x+i);
-    __m256 vectorY = _mm256_load_ps(y+i);
-    __m256 vectorXa = _mm256_mul_ps(vectorX, vectorA);
-    __m256 vectorYb = _mm256_mul_ps(vectorY, vectorB);
-    __m256 vectorW = _mm256_add_ps(vectorYb, vectorXa);
-    _mm256_store_ps(w+i, vectorW);
+    __m128 vectorX = _mm_load_ps(x+i);
+    __m128 vectorY = _mm_load_ps(y+i);
+    __m128 vectorXa = _mm_mul_ps(vectorX, vectorA);
+    __m128 vectorYb = _mm_mul_ps(vectorY, vectorB);
+    __m128 vectorW = _mm_add_ps(vectorYb, vectorXa);
+    _mm_store_ps(w+i, vectorW);
   }
-  // for(; i < n; i++){
-  //   w[i] = alpha * x[i] + beta * y[i];
-  // }
+  for(i=loopN; i < n; i++){
+    w[i] = alpha * x[i] + beta * y[i];
+  }
   return 0;
 }
 //The loop is very inefficient because the else encapsulates the whole loop essentially
